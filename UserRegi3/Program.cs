c745496dfd0d5c4c03b604573292bed3da1b9845
Harry <hareshwarp649>
2022-03-11 15:53:03 +0530

    Create User valid Email

---
+++ UserRegi3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseRegistration3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter Email ID: ");   //taking user input of Email ID
+            string Email = Console.ReadLine();
+            UserEmail.EmailId(Email);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UserRegi3/UserEmail.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UseRegistration3
+{
+    internal class UserEmail
+    {
+        public static void EmailId(string mail)
+        {
+            string emailid = "^[a-z]{3}[.][a-z0-9]*[@]{1}[bl]{2}[.]{1}[co]{2}[.]{1}[a-z]*$";
+            Regex regex = new Regex(emailid);
+
+            if (regex.IsMatch(mail))
+            {
+                Console.WriteLine("Email Id is valid");
+            }
+            else
+            {
+                Console.WriteLine("Email Is Invalid,Please Enter Valid email Id\n Refer and try again: abc.xyz@bl.co.in");
+            }
+
+        }
+    }
+}
