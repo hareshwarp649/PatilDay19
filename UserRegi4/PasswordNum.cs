4445b8de3cde479f4bba4524067896866f267d4b
Harry <hareshwarp649>
2022-03-11 16:00:14 +0530

    Print User valid password

---
+++ UserRegi4/PasswordNum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+
+namespace UserRegistration4
+{
+    internal class PasswordNum
+    {
+        public static void Password(string pass)
+        {
+            string pwd = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+            Regex regex = new Regex(pwd);                                           //creating a regex named obj with regular pwd.
+            if (regex.IsMatch(pass))
+            {
+                Console.WriteLine(pass + " is valid.");
+            }
+            else
+                Console.WriteLine(pass + " is invalid. Password must contain atleast one capital letter,one number,one special character");
+        }
+    }
+}
---
+++ UserRegi4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter password: ");   //taking user input of password1
+            string password1 = Console.ReadLine();
+            PasswordNum.Password(password1);
+            Console.ReadLine();
+
+        }
+    }
+}
