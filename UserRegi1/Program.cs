0e5a64cfe48da0e49b64aa1fe474ce03af998a53
Harry <hareshwarp649>
2022-03-11 15:28:44 +0530

    Create User first name

---
+++ UserRegi1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to validating User Registration using Regex");
+            Console.WriteLine("Enter Option " + "\n 1. Validating First Name" + "\n 2. Validating  Last Name");
+
+            UserFirstName name = new UserFirstName();
+            int atom= Convert.ToInt32(Console.ReadLine());
+           switch (atom)
+            {
+                case 1:
+                    name.FirstName();
+                    break;
+              case 2:
+                    name.LastName();
+                   break;
+           }
+        }
+    }
+}
---
+++ UserRegi1/UserFirstName.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    class UserFirstName
+
+    {
+
+      public  string pattern ="^[A-Z]{1}[a-zA-Z]{2}$";
+            //"^[A - Za - z]{3,}$";
+            //(@"^[a-zA-Z0-9]+([\.\+\-][a-zA-Z0-9]+)?@[a-zA-Z0-9-]+(\.[a-zA-Z]{2,}(\.[a-zA-Z]+)?)$");
+        public void FirstName()
+        {
+            Console.WriteLine("Validating the First Name");
+            Regex atom = new Regex(pattern);
+            Console.WriteLine("Enter the First Name");
+            string input = Console.ReadLine();
+            bool result = atom.IsMatch(input);
+            if (result)
+            {
+
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+
+
+          
+        public void LastName()
+        {
+            Console.WriteLine("Validating the Last Name");
+            Regex regex = new Regex(pattern);
+            Console.WriteLine("Enter the Last Name");
+            string input = Console.ReadLine();
+            bool result = regex.IsMatch(input);
+            if (result)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+        }
+    }
+}
